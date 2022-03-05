737f59b952d4fceefc9645f2aeceb675b000b6f2
yamatatsu <the.kinnikuman@gmail.com>
2022-03-05 11:12:21 +0900

    codehike

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import { remarkCodeHike } from "@code-hike/mdx";
+import theme from "./theme.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({ providerImportSource: "@mdx-js/react" })],
+  plugins: [
+    react(),
+    mdx({
+      remarkPlugins: [[remarkCodeHike, { theme }]],
+      pageExtensions: ["ts"],
+    }),
+  ],
+  resolve: {
+    alias: { "react/jsx-runtime": "react/jsx-runtime.js" },
+  },
   build: {
     minify: false,
   },
